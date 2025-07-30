1c2a2d81656b5cab947447416864449a542ac245
Federico Joaquin Rivero <federico.frivero+meli@mercadolibre.com>
2025-07-29 21:33:44 -0300

    Whatsapp Widget

--- src/components/layout/src/Layout.js
+++ src/components/layout/src/Layout.js
@@ -1,9 +1,30 @@
 import NavBar from '../../navBar/index';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
+import logoWP from '../../../images/logoWP.jpg';
 
 const Layout = ({ children }) => (
   <div className="app-container">
     <NavBar />
     {children}
+    <FloatingWhatsApp 
+      phoneNumber="5493513719249"
+      accountName="Dodo Travel"
+      chatMessage="Hola, ¿en qué puedo ayudarte?"
+      avatar={logoWP}
+      allowEsc
+      allowClickAway
+      notification
+      notificationSound
+      notificationTitle="Hola, ¿en qué puedo ayudarte?"
+      notificationSubTitle="Hola, ¿en qué puedo ayudarte?"
+      notificationBadge="Hola, ¿en qué puedo ayudarte?"
+      notificationBadgeColor="red"
+      notificationBadgeBackgroundColor="red"
+      notificationBadgeTextColor="white"
+      notificationBadgeFontSize="12px"
+      placeholder="Hola, queria consultar sobre ..."
+      statusMessage='Te ayudamos a volar'
+    />
   </div>
 );
 
